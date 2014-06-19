35cbb688aa013d31d5e770996ad8fd2f4fd19fe3
Stepanov Konstantin <kostja.stepanov@gmail.com>
2014-06-19 21:45:35 +0300

    initial commit

---
+++ app/views.py
@@ -0,0 +1,14 @@
+from flask import render_template
+from app import app
+
+@app.route('/')
+@app.route('/index')
+def index():
+	return render_template('index.html')
+
+@app.route('/user/<name>')
+def user(name):
+	return render_template('user.html', name=name)
+
+
+
